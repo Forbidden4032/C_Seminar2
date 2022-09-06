5be16c6a61a6c0e528ca3774be36c0d55a18dd70
forbidden <forbidden@fedora.com>
2022-09-06 23:25:20 +0300

    add ex_15

---
+++ ex_15/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+string WeekendCheck(int num)
+    {
+        if (num == 7) return "Воскресенье, выходной";
+        if (num == 6) return "Суббота, выходной";
+        if (num == 5) return "Пятница, рабочий день";
+        if (num == 4) return "Четверг, рабочий день";
+        if (num == 3) return "Среда, рабочий день";
+        if (num == 2) return "Вторник, рабочий день";
+        if (num == 1) return "Понедельник, рабочий день";
+        return "Несуществует";       
+    }
+
+
+Console.WriteLine("Введите цифру дня недели: ");
+int num = int.Parse(Console.ReadLine());
+
+string WkShow = WeekendCheck(num);
+Console.WriteLine($"Указанный день недели {num}: {WkShow}");
