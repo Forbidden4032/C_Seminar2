3d19c6b158ab0f320020e7141694067c2dca064c
forbidden <forbidden@fedora.com>
2022-09-06 22:58:00 +0300

    add ex_13

---
+++ ex_13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x > 99)
+    {
+
+    while (x >= 1000) 
+        { x = x / 10; 
+        } 
+    int result = x % 10; 
+    Console.WriteLine(result);
+    }
+else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+
