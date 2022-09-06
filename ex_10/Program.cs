3a663b572fc07f69d3f804b63d38c1bb01bf74c4
forbidden <forbidden@fedora.com>
2022-09-06 20:24:20 +0300

    add ex_10

--- ex_10/Program.cs
+++ ex_10/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+void ShowDigit (int x)
+    {
+    if (x < 999 && x > 99) 
+        {int y = (x%100)/10;
+        Console.WriteLine($"Трехзначное число {x}, второе число {y}");
+        }
+    else
+        {
+        Console.WriteLine($"Число {x} не трехзначное"); 
+        }
+    }
+
+Console.WriteLine("Введите трехзначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+ShowDigit(x);
+
